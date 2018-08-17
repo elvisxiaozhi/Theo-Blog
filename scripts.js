0c7977c7385baf754e4c980a37276572bbd3f567
Theodore <elvisxiaozhi@gmail.com>
2018-08-17 17:02:01 +0800

    add scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function myFunction(index) {
+   	var links = document.getElementById("navigation_bar").querySelectorAll("a");
+   	
+   	for(var i = 0; i < links.length; i++) {
+   		if(i === index) {
+   			links[i].style.color = "#AAAAAA";
+   		}
+   		else {
+   			links[i].style.color = "#666666";
+   		}
+   	}
+}
