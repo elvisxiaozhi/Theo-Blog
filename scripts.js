2272dd61483dfaa34c90d11abb724d2c9979b89e
Theodore <elvisxiaozhi@gmail.com>
2018-08-21 14:47:10 +0800

    add save cookie

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
-function myFunction(index) {
-   	var links = document.getElementById("navigation_bar").querySelectorAll("a");
+// function navBarClicked(index) {
+//    	var links = document.getElementById("navigation-bar").querySelectorAll("a");
    	
-   	for(var i = 0; i < links.length; i++) {
-   		if(i === index) {
-   			links[i].style.color = "#AAAAAA";
-   		}
-   		else {
-   			links[i].style.color = "#666666";
-   		}
-   	}
+//    	for(var i = 0; i < links.length; i++) {
+//    		if(i === index) {
+//    			links[i].style.color = "#AAAAAA";
+//    		}
+//    		else {
+//    			links[i].style.color = "#666666";
+//    		}
+//    	}
+// }
+
+function setCookie(name, value, exdays) {
+    var date = new Date();
+    date.setTime(date.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    alert(document.cookie);
 }
+
+function readCookie() {
+	alert(document.cookie);
+}
+
+$(document).ready(function() {
+    $("#navigation-bar.a").click(function() {
+        $("#navigation-bar.a").removeClass('current');
+        $(this).addClass('current');
+    });
+});
 
 $(function(){
   $("#nav-placeholder").load("nav.html");
